2c020749db3d73225111721b1b26d167bb0970ab
janibehm <jani.behm@gmail.com>
2023-04-04 02:32:41 +0300

    cleaning code

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// Define an array of generation limits and offsets
-/* const generations = [
-  { limit: 151, offset: 0 },
-  { limit: 100, offset: 151 },
-  { limit: 135, offset: 251 },
-  { limit: 107, offset: 386 },
-  { limit: 156, offset: 493 },
-  { limit: 72, offset: 649 },
-  { limit: 88, offset: 721 },
-  { limit: 96, offset: 809 },
-  { limit: 3, offset: 905 }
-] */
-// Get all the buttons and add click event listeners
-/* const buttons = document.querySelectorAll('.buttons')
-let clickedButtonId = 0
-
-buttons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    clickedButtonId = index
-    console.log(`Button with id ${clickedButtonId} was clicked!`)
-    fetchGenerations()
-  })
-}) */
-
 const generations = [
   { limit: 151, offset: 0 },
   { limit: 100, offset: 151 },
